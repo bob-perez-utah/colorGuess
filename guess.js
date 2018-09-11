0e628b7744ce3cc1794fa85a493bc3546072eb45
bob-perez-utah <bperez@gw.novell.com>
2018-09-10 20:14:44 -0600

    initial commit

--- guess.js
+++ guess.js
@@ -38,7 +38,7 @@ window.setTimeout (function() {
         for (var i = 0; i < squares.length; i++) {
             squares[i].style.background = colors[i];
         }
-        h1.style.background = "#232323";
+        h1.style.background = "steelblue";
         messageDisplay.textContent = "";
         resetButton.textContent = "New Colors";
     });
